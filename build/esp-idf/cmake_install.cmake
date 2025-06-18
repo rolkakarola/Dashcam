7a27b8ca4593f245df0fa6390615ebb0172ecdc5
SroloRolo <rolkakarola@gmail.com>
2025-06-18 20:46:21 +0200

    project now working

--- build/esp-idf/cmake_install.cmake
+++ build/esp-idf/cmake_install.cmake
@@ -564,16 +564,6 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("/Users/karolinarola/Documents/EspIDF/ESP32-CAM-ESP-IDF-Live-Streaming-Web-Server/build/esp-idf/espressif__onewire_bus/cmake_install.cmake")
-endif()
-
-if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  # Include the install script for the subdirectory.
-  include("/Users/karolinarola/Documents/EspIDF/ESP32-CAM-ESP-IDF-Live-Streaming-Web-Server/build/esp-idf/espressif__ds18b20/cmake_install.cmake")
-endif()
-
-if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  # Include the install script for the subdirectory.
   include("/Users/karolinarola/Documents/EspIDF/ESP32-CAM-ESP-IDF-Live-Streaming-Web-Server/build/esp-idf/main/cmake_install.cmake")
 endif()
 
--- main/main.c
+++ main/main.c
@@ -2,43 +2,22 @@
 #include <nvs_flash.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-
 #include "driver/gpio.h"
-#include "driver/sdmmc_host.h"
-#include "driver/sdmmc_defs.h"
-#include "sdmmc_cmd.h"
-#include "esp_vfs_fat.h"
 
 #include "esp_camera.h"
+#include "esp_http_server.h"
+#include "esp_timer.h"
+#include "camera_pins.h"
+#include "connect_wifi.h"
 
-#define BUTTON_PIN 3
+static const char *TAG = "esp32-cam Webserver";
 
-static void button_handler(void *arg);
-static void take_photo(void *arg);
-
-#define CAM_PIN_PWDN    32 
-#define CAM_PIN_RESET   -1 //software reset will be performed
-#define CAM_PIN_XCLK    0
-#define CAM_PIN_SIOD    26
-#define CAM_PIN_SIOC    27
-
-#define CAM_PIN_D7      35
-#define CAM_PIN_D6      34
-#define CAM_PIN_D5      39
-#define CAM_PIN_D4      36
-#define CAM_PIN_D3      21
-#define CAM_PIN_D2      19
-#define CAM_PIN_D1      18
-#define CAM_PIN_D0       5
-#define CAM_PIN_VSYNC   25
-#define CAM_PIN_HREF    23
-#define CAM_PIN_PCLK    22
+#define PART_BOUNDARY "123456789000000000000987654321"
+static const char* _STREAM_CONTENT_TYPE = "multipart/x-mixed-replace;boundary=" PART_BOUNDARY;
+static const char* _STREAM_BOUNDARY = "\r\n--" PART_BOUNDARY "\r\n";
+static const char* _STREAM_PART = "Content-Type: image/jpeg\r\nContent-Length: %u\r\n\r\n";
 
 #define CONFIG_XCLK_FREQ 20000000 
-#define CONFIG_OV2640_SUPPORT 1
-#define CONFIG_OV7725_SUPPORT 1
-#define CONFIG_OV3660_SUPPORT 1
-#define CONFIG_OV5640_SUPPORT 1
 
 static esp_err_t init_camera(void)
 {
@@ -79,100 +58,115 @@ static esp_err_t init_camera(void)
     return ESP_OK;
 }
 
-static esp_err_t initi_sd_card(void)
-{  
-    sdmmc_host_t host = SDMMC_HOST_DEFAULT();
-    sdmmc_slot_config_t slot_config = SDMMC_SLOT_CONFIG_DEFAULT();
-    esp_vfs_fat_sdmmc_mount_config_t mount_config = {
-        .format_if_mount_failed = false,
-        .max_files = 3,
-    };
-    sdmmc_card_t *card;
-    esp_err_t err = esp_vfs_fat_sdmmc_mount("/sdcard", &host, &slot_config, &mount_config, &card);
-    if (err != ESP_OK)
-    {
-        return err;
+esp_err_t jpg_stream_httpd_handler(httpd_req_t *req){
+    camera_fb_t * fb = NULL;
+    esp_err_t res = ESP_OK;
+    size_t _jpg_buf_len;
+    uint8_t * _jpg_buf;
+    char * part_buf[64];
+    static int64_t last_frame = 0;
+    if(!last_frame) {
+        last_frame = esp_timer_get_time();
     }
-    return ESP_OK;
+
+    res = httpd_resp_set_type(req, _STREAM_CONTENT_TYPE);
+    if(res != ESP_OK){
+        return res;
+    }
+
+    while(true){
+        fb = esp_camera_fb_get();
+        if (!fb) {
+            ESP_LOGE(TAG, "Camera capture failed");
+            res = ESP_FAIL;
+            break;
+        }
+        if(fb->format != PIXFORMAT_JPEG){
+            bool jpeg_converted = frame2jpg(fb, 80, &_jpg_buf, &_jpg_buf_len);
+            if(!jpeg_converted){
+                ESP_LOGE(TAG, "JPEG compression failed");
+                esp_camera_fb_return(fb);
+                res = ESP_FAIL;
+            }
+        } else {
+            _jpg_buf_len = fb->len;
+            _jpg_buf = fb->buf;
+        }
+
+        if(res == ESP_OK){
+            res = httpd_resp_send_chunk(req, _STREAM_BOUNDARY, strlen(_STREAM_BOUNDARY));
+        }
+        if(res == ESP_OK){
+            size_t hlen = snprintf((char *)part_buf, 64, _STREAM_PART, _jpg_buf_len);
+
+            res = httpd_resp_send_chunk(req, (const char *)part_buf, hlen);
+        }
+        if(res == ESP_OK){
+            res = httpd_resp_send_chunk(req, (const char *)_jpg_buf, _jpg_buf_len);
+        }
+        if(fb->format != PIXFORMAT_JPEG){
+            free(_jpg_buf);
+        }
+        esp_camera_fb_return(fb);
+        if(res != ESP_OK){
+            break;
+        }
+        int64_t fr_end = esp_timer_get_time();
+        int64_t frame_time = fr_end - last_frame;
+        last_frame = fr_end;
+        frame_time /= 1000;
+        ESP_LOGI(TAG, "MJPG: %uKB %ums (%.1ffps)",
+            (uint32_t)(_jpg_buf_len/1024),
+            (uint32_t)frame_time, 1000.0 / (uint32_t)frame_time);
+    }
+
+    last_frame = 0;
+    return res;
 }
 
-static esp_err_t init_button_isr(void)
+httpd_uri_t uri_get = {
+    .uri = "/",
+    .method = HTTP_GET,
+    .handler = jpg_stream_httpd_handler,
+    .user_ctx = NULL};
+httpd_handle_t setup_server(void)
 {
-    gpio_config_t io_conf;
-    io_conf.mode = GPIO_MODE_INPUT;
-    io_conf.pin_bit_mask = (1ULL << BUTTON_PIN);
-    io_conf.intr_type = GPIO_INTR_POSEDGE;
-    io_conf.pull_up_en = 1;
-    esp_err_t err = gpio_config(&io_conf);
-    if (err != ESP_OK)
+    httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+    httpd_handle_t stream_httpd  = NULL;
+
+    if (httpd_start(&stream_httpd , &config) == ESP_OK)
     {
-        return err;
+        httpd_register_uri_handler(stream_httpd , &uri_get);
     }
-    return gpio_isr_handler_add(BUTTON_PIN, button_handler, NULL);
-}
 
-static TickType_t next = 0;
-const TickType_t period = 2000 / portTICK_PERIOD_MS;
-
-static void IRAM_ATTR button_handler(void *arg)
-{
-    TickType_t now = xTaskGetTickCountFromISR();
-
-    if (now > next)
-    {
-        xTaskCreate(take_photo, "pic", configMINIMAL_STACK_SIZE * 5, NULL, 5, NULL);
-    }
-    next = now + period;
-}
-
-static void take_photo(void *arg)
-{
-    printf("Starting Taking Picture!\n");
-
-    camera_fb_t *pic = esp_camera_fb_get();
-
-    char photo_name[50];
-    //sprintf(photo_name, "/sdcard/pic_%li.jpg", pic->timestamp.tv_sec);
-
-    FILE *file = fopen(photo_name, "w");
-    if (file == NULL)
-    {
-        printf("err: fopen failed\n");
-    }
-    else
-    {
-        fwrite(pic->buf, 1, pic->len, file);
-        fclose(file);
-    }
-    
-    esp_camera_fb_return(pic);
-    vTaskDelete(NULL);
-    printf("Finished Taking Picture!\n");
+    return stream_httpd;
 }
 
 void app_main()
 {
     esp_err_t err;
-    err = init_camera();
-    if (err != ESP_OK)
+
+    // Initialize NVS
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
     {
-        printf("err: %s\n", esp_err_to_name(err));
-        return;
-    }
-    
-    err = initi_sd_card();
-    if (err != ESP_OK)
-    {
-        printf("err: %s\n", esp_err_to_name(err));
-        return;
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
     }
 
-    err = init_button_isr();
-    if (err != ESP_OK)
-    {
-        printf("err: %s\n", esp_err_to_name(err));
-        return;
-    }
+    connect_wifi();
 
-    printf("Everything is initialized successfully - Press Push button to take Picture\n");
+    if (wifi_connect_status)
+    {
+        err = init_camera();
+        if (err != ESP_OK)
+        {
+            printf("err: %s\n", esp_err_to_name(err));
+            return;
+        }
+        setup_server();
+        ESP_LOGI(TAG, "ESP32 CAM Web Server is up and running\n");
+    }
+    else
+        ESP_LOGI(TAG, "Failed to connected with Wi-Fi, check your network Credentials\n");
 }
